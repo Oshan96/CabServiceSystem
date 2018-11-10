f66ea9ce23eb2060c50f5620f4c64d6a59bb124d
Oshan96 <oshanmen@gmail.com>
2018-11-10 09:12:59 +0530

    Updated adminNew (need to do color changes)

--- js/navigationevent.js
+++ js/navigationevent.js
@@ -1,22 +1,22 @@
 
 var isShown = true;
 
-function executeEvent() {
-    console.log(isShown)
-    // if(isShown) {
-    //     document.getElementById("first").style.width = '0%'
-    //     document.getElementById("first").style.display = 'none'
-    //     document.getElementById("second").style.width = '100%'
-    // } else {
-    //     document.getElementById("first").style.width = '41.66%'
-    //     document.getElementById("first").style.display = 'flex'
-    //     document.getElementById("second").style.width = '58.33%'
+// function executeEvent() {
+//     console.log(isShown)
+//     if(isShown) {
+//         document.getElementById("first").style.width = '0%'
+//         document.getElementById("first").style.display = 'none'
+//         document.getElementById("second").style.width = '100%'
+//     } else {
+//         document.getElementById("first").style.width = '41.66%'
+//         document.getElementById("first").style.display = 'flex'
+//         document.getElementById("second").style.width = '58.33%'
 
-    // }
+//     }
 
-    isShown = !isShown
+//     isShown = !isShown
 
-}
+// }
 
 window.onscroll = function() {
     var height = document.getElementById("navbar").style.height;
